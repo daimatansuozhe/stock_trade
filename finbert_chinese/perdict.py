f134ab4186356c11145c11cd4c144e42c4ad08a0
lemon_lv <874712205@qq.com>
2025-04-16 11:23:51 +0800

    增加了自定义modeling_finbert.py

--- finbert_chinese/perdict.py
+++ finbert_chinese/perdict.py
@@ -1,42 +1,61 @@
-from transformers import TextClassificationPipeline, AutoModelForSequenceClassification, BertTokenizerFast
+from transformers import AutoModelForSequenceClassification, BertTokenizerFast,AutoConfig
 import pandas as pd
 import os
+import numpy as np
+import torch
+
 # ========== 1. 模型与Tokenizer加载 ==========
 #联网调用yiyanghkust/finbert-tone-chinese，github中未上传local_model，选这个
 # model_path = "yiyanghkust/finbert-tone-chinese"
 # model = AutoModelForSequenceClassification.from_pretrained(model_path)
 # tokenizer = BertTokenizerFast.from_pretrained(model_path)
-local_path='./local_model/finbert_chinese'
-model = AutoModelForSequenceClassification.from_pretrained(local_path)
-tokenizer = BertTokenizerFast.from_pretrained(local_path)
-#device=0 表示第 0 块 GPU,device=-1 表示使用CPU
-classifier = TextClassificationPipeline(model=model, tokenizer=tokenizer,top_k=None,device=-1)
+# classifier = TextClassificationPipeline(model=model, tokenizer=tokenizer,top_k=None,device=-1)
 
-# ========== 2. 读取CSV文件 ==========
-input_file = "input_sentences.csv"  # 假设CSV中有列名 sentence
-df_input = pd.read_csv(input_file)
 
-# 检查是否存在 sentence 列
-if "sentence" not in df_input.columns:
-    raise ValueError("CSV文件必须包含'sentence'这一列。")
 
-# ========== 3. 情感预测 ==========
-results = []
-for sentence in df_input["sentence"]:
-    prediction_scores = classifier(sentence)[0]
-    top_result = max(prediction_scores, key=lambda x: x['score'])
-    results.append({
-        'sentence': sentence,
-        'prediction': top_result['label'],
-        'sentiment_score': round(top_result['score'], 4)
-    })
 
-# ========== 4. 保存为新的CSV文件 ==========
-df_output = pd.DataFrame(results)
-if not os.path.exists('./output'):
-    os.mkdir('./output')
+# 分类函数
+def classify(texts):
+    result=[]
+    for text in texts:
+        inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True)
+        with torch.no_grad():
+            outputs = model(**inputs)
+            logits = outputs.logits
+            predicted_class_id = logits.argmax().item()
+            confidence = torch.softmax(logits, dim=1)[0, predicted_class_id].item()
+        label = config.id2label.get(predicted_class_id, str(predicted_class_id))
+        result.append({
+            "Text": text,
+            "Predicted Label": label,
+            "Confidence": round(confidence, 4)
+        })
+        df=pd.DataFrame(result)
+    return df
 
-output_file = "./output/finbert_ch_pre.csv"
-df_output.to_csv(output_file, index=False, encoding='utf-8-sig')
 
-print(f"✅ 情感分析完成，结果已保存为：{output_file}")
+def write_to_csv(df, path="./output",file="finbert_ch_pre1.csv"):
+    df.to_csv(os.path.join(path,file), index=False)
+    print(f"✅ 分类结果已保存至 {os.path.join(path,file)}")
+
+
+
+
+if __name__=="__main__":
+    #本地配置储存path
+    local_path = './local_model/finbert_chinese'
+    config = AutoConfig.from_pretrained(local_path)
+    #加载预训练的tokenizer，model，“trust_remote_code=True”：model采用modeling_finbert.py
+    tokenizer = BertTokenizerFast.from_pretrained(local_path)
+    model = AutoModelForSequenceClassification.from_pretrained(local_path, config=config, trust_remote_code=True)
+
+    input_file = "input_sentences.csv"  # 假设CSV中有列名 sentence
+    df_input = pd.read_csv(input_file)
+    texts = df_input["sentence"].tolist()  #['此外宁德时代上半年实现出口约2GWh，同比增加200%+。', '今日股市波动剧烈，投资者信心不足。']
+
+    model.eval()
+    #情感分析判断
+    df=classify(texts)
+    write_to_csv(df)
+
+
