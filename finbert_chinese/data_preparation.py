740b2fbed69137bd0db893f75cdf7068d9c9c299
lemon_lv <874712205@qq.com>
2025-04-17 16:33:18 +0800

    更新

--- finbert_chinese/data_preparation.py
+++ finbert_chinese/data_preparation.py
@@ -3,7 +3,8 @@ import pandas as pd
 
 def load_sentences(file_path):
     # header=None：表示原始文件没有表头行（第一行就是数据，不是列名）
-    df = pd.read_csv(file_path, sep='@', names=["sentence", "label"],encoding='ISO-8859-1')
+    #对。txt进行格式转换： sentence@positive
+    df = pd.read_csv(file_path, sep='@', names=["sentence", "label"],header=None,encoding='ISO-8859-1')
     df["sentence"] = df["sentence"].str.strip()  # 去除前后空格
     df["label"]=df["label"].str.strip()
     sentence_label = df["sentence"].tolist()
--- finbert_chinese/dp_dfc.py
+++ finbert_chinese/dp_dfc.py
@@ -3,7 +3,10 @@ import pandas as pd
 def dfc_load(file_path):
     df = pd.read_csv(file_path)
     df1 = pd.DataFrame()
+
+    #sentence为标题➕正文⬇️
     # df1['sentence'] = df['标题'] + ':' + df['正文']
+    #sentence仅为标题⬇️
     df1['sentence'] = df['标题']
     df1['label_num'] = df['正负面']
     # header=None：表示原始文件没有表头行（第一行就是数据，不是列名）
@@ -15,5 +18,4 @@ def dfc_load(file_path):
     for i in df1['label_num']:
         label.append('Negative') if i==0 else label.append('Positive')
     df1['label']=pd.DataFrame(label)
-    print()
     return sentence_label,df1["label"]
